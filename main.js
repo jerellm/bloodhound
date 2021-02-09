d8d32f3ddbeddaa10dbd80344eea4f21a05e7ddc
jerell <jmacarae@ucsc.edu>
2021-02-08 18:12:01 -0800

    Pushing changes to test on windows. Testing console log functionality and add tracking number button functionality.

--- main.js
+++ main.js
@@ -1,43 +1,111 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+let mainWindow;
+let addWindow;
+// Listen for app to be ready
+app.on('ready', function(){
+	// Create new window
+	mainWindow = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	// Load html into window
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol:'file:',
+		slashes: true
+	}));
+	// Quit app when closed
+	mainWindow.on('closed', function(){
+		app.quit();
+	});
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	// Insert menu
+	Menu.setApplicationMenu(mainMenu);
+});
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+// Handle createAddWindow()
+function createAddWindow(){
+	// Create new window
+	addWindow = new BrowserWindow({
+		width: 300, 
+		height:200,
+		title:'Add tracking number',
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	// Load html into window
+	addWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol:'file:',
+		slashes: true
+	}));
+	// Garbage collection handle
+	addWindow.on('close', function(){
+		addWindow = null;
+	})
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+// Catch item:add
+ipcMain.on('item.add', function(e, item){
+	console.log(item);
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
+});
+// Create menu template
+const mainMenuTemplate = [
+	{
+		label:'File',
+		submenu:[
+			{
+				label: 'Add Item',
+				click(){
+					createAddWindow();
+				}
+			},
+			{ 
+				label: 'Clear Items'
+			},
+			{
+				label: 'Quit',
+				// Add keyboard shortcuts for Mac and Windows/Linux
+				accelerator: process.platform == 'darwin' ? 'Command+Q' :
+				'Ctrl+Q',
+				click(){app.quit();
+				}
+			}
+		]
+	}
+];
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+// If Mac add empty object to menu
+if(process.platform == 'darwin'){
+	mainMenuTemplate.unshift({});
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Add developer tools if not in prodution
+if(process.env.NODE_ENV !== 'production'){
+	mainMenuTemplate.push({
+		label: 'Developer tools',
+		submenu:[
+			{
+				label: 'Toggle DevTools',
+				accelerator: process.platform == 'darwin' ? 'Command+I' :
+				'Ctrl+I',
+				click(item, focusedWindow){
+					focusedWindow.toggleDevTools();
+				}
+			},
+			{
+				role: 'reload'
+			}
+		]
+	});
+}
